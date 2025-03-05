26f431c85a2e833ec52dc1e0a63a6b7253374957
Tolkun Viktor <v.s.tolkun@gmail.com>
2025-03-05 10:18:27 +0300

    Add SDDM module with installation and uninstallation support
    - Create packages.txt for SDDM and SDDM KDE configuration manager
    - Implement utils.sh script for SDDM module installation and uninstallation
    - Enable/disable SDDM systemd service during installation/uninstallation
    - Follow standardized module installation script structure

---
+++ modules/system/sddm/utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Global variables
+REP_ROOT="$(git rev-parse --show-toplevel)"
+MODULE_DIR="$(dirname "$0")"
+
+if [ "$1" == "install" ]; then
+    source "${REP_ROOT}/scripts/installation.sh"
+    install_packages "${MODULE_DIR}/packages.txt"
+    sudo systemctl enable sddm
+fi
+
+if [ "$1" == "uninstall" ]; then
+    sudo systemctl disable sddm
+    source "${REP_ROOT}/scripts/cleaning.sh"
+    remove_packages "${MODULE_DIR}/packages.txt"
+fi
