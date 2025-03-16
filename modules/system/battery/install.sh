32eb6a98360a0e09e353d392fd178373a843aefd
Tolkun Viktor <v.s.tolkun@gmail.com>
2025-03-16 09:43:25 +0000

    Add battery module

---
+++ modules/system/battery/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Global variables
+REP_ROOT="$(git rev-parse --show-toplevel)"
+MODULE_DIR="$(dirname "$0")"
+
+# Install
+if [ "$1" == "install" ]; then
+    source "${REP_ROOT}/scripts/installation.sh"
+    install_packages "${MODULE_DIR}/packages.txt"
+fi
+
+# Uninstall
+if [ "$1" == "uninstall" ]; then
+    source "${REP_ROOT}/scripts/cleaning.sh"
+    remove_packages "${MODULE_DIR}/packages.txt"
+fi
