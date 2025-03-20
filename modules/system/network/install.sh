7f2843803259bee383e1573ed83a480d6d7e3c3a
Tolkun Viktor <v.s.tolkun@gmail.com>
2025-03-21 01:41:47 +0300

    add vpn configurations

--- modules/system/network/install.sh
+++ modules/system/network/install.sh
@@ -8,10 +8,16 @@ MODULE_DIR="$(dirname "$0")"
 if [ "$1" == "install" ]; then
     source "${REP_ROOT}/scripts/installation.sh"
     install_packages "${MODULE_DIR}/packages.txt"
+    sudo wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
+    sudo chmod +x "/etc/openvpn/update-resolv-conf"
+    mkdir $HOME/.openvpn
+    cd $HOME/.openvpn
+    copy_configs "${MODULE_DIR}/configs" "$HOME/.openvpn"
 fi
 
 # Uninstall
 if [ "$1" == "uninstall" ]; then
     source "${REP_ROOT}/scripts/cleaning.sh"
     remove_packages "${MODULE_DIR}/packages.txt"
+    remove_configs "${MODULE_DIR}/configs" "$HOME/.openvpn"
 fi
