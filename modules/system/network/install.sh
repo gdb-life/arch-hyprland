498cd3289884768e7835564b17b07b23b7610fd6
Tolkun Viktor <v.s.tolkun@gmail.com>
2025-03-21 13:40:01 +0300

    Add vpn configs and files with auto auth

--- modules/system/network/install.sh
+++ modules/system/network/install.sh
@@ -3,21 +3,35 @@
 # Global variables
 REP_ROOT="$(git rev-parse --show-toplevel)"
 MODULE_DIR="$(dirname "$0")"
+VPN_DIR="$HOME/.openvpn"
 
 # Install
 if [ "$1" == "install" ]; then
     source "${REP_ROOT}/scripts/installation.sh"
     install_packages "${MODULE_DIR}/packages.txt"
+
+    # proton-vpn
     sudo wget "https://raw.githubusercontent.com/ProtonVPN/scripts/master/update-resolv-conf.sh" -O "/etc/openvpn/update-resolv-conf"
     sudo chmod +x "/etc/openvpn/update-resolv-conf"
-    mkdir $HOME/.openvpn
-    cd $HOME/.openvpn
-    copy_configs "${MODULE_DIR}/configs" "$HOME/.openvpn"
+    if [ ! -d $VPN_DIR ]; then
+        mkdir $VPN_DIR
+    fi
+    copy_configs "${MODULE_DIR}/configs" "$VPN_DIR"
+    read -rp "enter proton login: " LOGIN
+    read -rp "enter proton password: " PASSWORD
+    for file in "$VPN_DIR"/*; do
+        if [ "$(basename "$file")" != "auth.txt" ]; then
+            echo "auth-user-pass auth.txt" >> $file
+        fi
+    done
+    echo "$LOGIN" >> "$VPN_DIR/auth.txt"
+    echo "$PASSWORD" >> "$VPN_DIR/auth.txt"
 fi
 
 # Uninstall
 if [ "$1" == "uninstall" ]; then
     source "${REP_ROOT}/scripts/cleaning.sh"
     remove_packages "${MODULE_DIR}/packages.txt"
-    remove_configs "${MODULE_DIR}/configs" "$HOME/.openvpn"
+    remove_configs "${MODULE_DIR}/configs" "$VPN_DIR"
+    sudo rm -rf "/etc/openvpn/update-resolv-conf"
 fi
