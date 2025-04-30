5462e09345ca046c80dc4e2ab0c074318bae862f
Viktor <v.s.tolkun@gmail.com>
2025-04-30 13:07:41 +0000

    Add audio and fonts modules

---
+++ modules/system/fonts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Global variables
+REP_ROOT="$(git rev-parse --show-toplevel)"
+MODULE_DIR="$(dirname "$0")"
+PACKAGES="${MODULE_DIR}/packages.txt"
+
+# Install
+if [ "$1" == "install" ]; then
+    source "${REP_ROOT}/scripts/installation.sh"
+    install_packages "${PACKAGES}"
+    fc-cache -f
+fi
+
+# Uninstall
+if [ "$1" == "uninstall" ]; then
+    source "${REP_ROOT}/scripts/cleaning.sh"
+    remove_packages "${PACKAGES}"
+fi
